14a2d5389e4ff17f182cf97707d4e7dcd7f38f71
Deepanshu Mittal <deepanshu.mittal@ingka.ikea.com>
2022-01-23 16:31:47 +0100

    customer and other qualification context added

---
+++ src/main/java/com/example/promotionengine/model/Customer.java
@@ -0,0 +1,23 @@
+package com.example.promotionengine.model;
+
+public class Customer {
+
+    private String[] customerType;
+    private String[] customerId;
+
+    public String[] getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String[] customerType) {
+        this.customerType = customerType;
+    }
+
+    public String[] getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String[] customerId) {
+        this.customerId = customerId;
+    }
+}
--- src/main/java/com/example/promotionengine/model/Promotion.java
+++ src/main/java/com/example/promotionengine/model/Promotion.java
@@ -10,6 +10,104 @@ public class Promotion {
     private Qualification qualifyingCondition;
     private Qualification targetItems;
     private DiscountRule discountRule;
+    private String[] countries;
+    private Integer redemptionLimit;
+    private Integer priority;
+    private Customer customerContext;
 
+    public String getPromotionName() {
+        return promotionName;
+    }
 
+    public void setPromotionName(String promotionName) {
+        this.promotionName = promotionName;
+    }
+
+    public String getPromotionCode() {
+        return promotionCode;
+    }
+
+    public void setPromotionCode(String promotionCode) {
+        this.promotionCode = promotionCode;
+    }
+
+    public String getPromotionDescription() {
+        return promotionDescription;
+    }
+
+    public void setPromotionDescription(String promotionDescription) {
+        this.promotionDescription = promotionDescription;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public Qualification getQualifyingCondition() {
+        return qualifyingCondition;
+    }
+
+    public void setQualifyingCondition(Qualification qualifyingCondition) {
+        this.qualifyingCondition = qualifyingCondition;
+    }
+
+    public Qualification getTargetItems() {
+        return targetItems;
+    }
+
+    public void setTargetItems(Qualification targetItems) {
+        this.targetItems = targetItems;
+    }
+
+    public DiscountRule getDiscountRule() {
+        return discountRule;
+    }
+
+    public void setDiscountRule(DiscountRule discountRule) {
+        this.discountRule = discountRule;
+    }
+
+    public String[] getCountries() {
+        return countries;
+    }
+
+    public void setCountries(String[] countries) {
+        this.countries = countries;
+    }
+
+    public Integer getRedemptionLimit() {
+        return redemptionLimit;
+    }
+
+    public void setRedemptionLimit(Integer redemptionLimit) {
+        this.redemptionLimit = redemptionLimit;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Customer getCustomerContext() {
+        return customerContext;
+    }
+
+    public void setCustomerContext(Customer customerContext) {
+        this.customerContext = customerContext;
+    }
 }
